7d9db7a7b9afa21eb3f42b811b944f070a909651
CarlosRocha2409 <carlos.eduardo.rocha.silva@gmail.com>
2021-09-16 22:59:59 -0600

    cd ci change

---
+++ scripts/application_start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#give permission for everything in the ga-aws directory
+sudo chmod -R 777 /home/ec2-user/ga-aws
+
+#navigate into our working directory where we have all our github files
+cd /home/ec2-user/ga-aws
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+
+#install pm2
+ npm install pm2@latest -g
+
+#install node modules
+npm install
+
+pm2 kill
+
+#start our node app in the background
+pm2 start index.js > app.out.log 2> app.err.log < /dev/null & 
+
+
